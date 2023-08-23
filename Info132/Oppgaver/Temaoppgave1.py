b0dc25fc809f282e84a27d4ab4d5bde151a30784
ellrub <ellefsen88@gmail.com>
2023-08-23 14:30:00 +0200

    Answered task 1

---
+++ Info132/Oppgaver/Temaoppgave1.py
@@ -0,0 +1,12 @@
+# Oppgave 1. Lag minst to ulike programmer som skriver navnet ditt på skjermen, med for- 
+# (mellom-) og etternavn på hver sin linje. (Hint: bruk forskjellige varianter av print()-funksjonen.)
+
+#------------- Program 1 -------------#
+# name = "Ruben"
+# lastname = "Ellefsen"
+
+# print(name)
+# print(lastname)
+
+#------------- Program 2 -------------#
+print("Ruben\nEllefsen")
