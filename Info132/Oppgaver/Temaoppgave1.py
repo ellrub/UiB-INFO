089319244f3998ed59ac272452925cd3129f0e38
ellrub <ellefsen88@gmail.com>
2023-08-24 10:41:04 +0200

    add:new program with a for loop

--- Info132/Oppgaver/Temaoppgave1.py
+++ Info132/Oppgaver/Temaoppgave1.py
@@ -2,14 +2,20 @@
 # (mellom-) og etternavn på hver sin linje. (Hint: bruk forskjellige varianter av print()-funksjonen.)
 
 #------------- Program 1 -------------#
+# print("Ruben\nEllefsen")
+
+#------------- Program 2 -------------#
 # name = "Ruben"
 # lastname = "Ellefsen"
 
 # print(name)
 # print(lastname)
 
-#------------- Program 2 -------------#
-# print("Ruben\nEllefsen")
+#------------- Program 3 -------------#
+# names = ["Ruben", "Ellefsen"]
+
+# for name in names:
+#     print(name)
 
 
 # Oppgave 2 
@@ -17,13 +23,13 @@
     # Du kan anta en fast kronekurs, f eks dollarkurs = 11.62 og eurokurs = 10.2. Utskriften skal se slik ut:
     # "250 norske kroner tilsvarer 25.62 Euro og 29.05 Dollar"
 
-krone = input("Antall kroner du vil gjøre om til Euro og Dollar: ")
-euro_exchange = 10.20
-dollar_exchange = 11.62
+# krone = input("Antall kroner du vil gjøre om til Euro og Dollar: ")
+# euro_exchange = 10.20
+# dollar_exchange = 11.62
 
-print(f"{krone} norske kroner tilsvarer {float(krone) / euro_exchange:.2f} Euro og {float(krone) / dollar_exchange:.2f} Dollar")
+# print(f"{krone} norske kroner tilsvarer {float(krone) / euro_exchange:.2f} Euro og {float(krone) / dollar_exchange:.2f} Dollar")
 
     # 2b)  Spesialtegnene € og $ kan skrives ved hjelp av tegnkoder, henholdsvis u"\N{euro sign}" og u"\N{dollar sign}".
     # Bruk disse til å lage penere utskrift som dette: 250 norske kroner tilsvarer 25.62€ og 29.05$
 
-print(f"{krone} norske kroner tilsvarer {float(krone) / euro_exchange:.2f}\N{euro sign} og {float(krone) / dollar_exchange:.2f}\N{dollar sign}")
+# print(f"{krone} norske kroner tilsvarer {float(krone) / euro_exchange:.2f}\N{euro sign} og {float(krone) / dollar_exchange:.2f}\N{dollar sign}")
