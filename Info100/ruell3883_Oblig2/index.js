9757102bb632f4a19893cd6746afc020edec0c46
Ruben Ellefsen <ellefsen88@gmail.com>
2023-10-22 04:32:19 +0200

    add: footer text displayed with JS

---
+++ Info100/ruell3883_Oblig2/index.js
@@ -0,0 +1,7 @@
+const footer = document.getElementById("contact")
+footer.innerHTML = `<p>Contact Information</p>
+                    <p>Phone: <a href="tel:+4712345678">+47 123 45 678</a></p>
+                    <p>e-mail: <a href="mailto:user@mail.com">user@mail.com</a></p>
+                    <p>Inspired by The Office Michael Scott (Steve Carell) and his business idea ShoeLala, that was never ready for the world</p>
+`
+
