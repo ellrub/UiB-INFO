57d618f800d305ea63b0b307a2d9567298bd92db
Ruben Ellefsen <ellefsen88@gmail.com>
2024-01-31 15:14:01 +0100

    .

--- Info135/Assignments/ruell3883_assignment01.py
+++ Info135/Assignments/ruell3883_assignment01.py
@@ -29,3 +29,33 @@ node_list.head = node1
 node1.next = node2
 node2.next = node3
 node_list.print_list()
+
+# Assignment 3
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+def reverse_list(my_list):
+    stack = Stack()
+    new_list = []
+    for item in my_list:
+        stack.push(item)
+    while not stack.is_empty():
+        new_list.append(stack.pop())
+
+    print(new_list)
+    
+my_list = [1, 2, 3, 4, 5]
+reverse_list(my_list)
+
