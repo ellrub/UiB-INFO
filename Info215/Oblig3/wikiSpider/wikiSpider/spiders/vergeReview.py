fe5bce53111a7c5fb63ffd8a632b5af687138d89
Ruben Ellefsen <ellefsen88@gmail.com>
2025-03-01 13:55:20 +0100

    .

--- Info215/Oblig3/wikiSpider/wikiSpider/spiders/vergeReview.py
+++ Info215/Oblig3/wikiSpider/wikiSpider/spiders/vergeReview.py
@@ -1,23 +1,26 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from wikiSpider.items import VergeReview
+from urllib.request import urljoin
 
 class VergeReviewSpider(CrawlSpider):
     name = 'vergeReview'
 
     allowed_domains = ['theverge.com']
     start_urls = ['https://www.theverge.com/reviews']
+    
 
     rules = [
-        Rule(LinkExtractor(allow = "https://www\.theverge\.com/(?:[a-z-]+/\d+|d+)/[^/]+"),
+        Rule(LinkExtractor(allow = r"https://www\.theverge\.com/\d+/[^/]+"),
              callback = 'parse_items', follow = True)
         ]
     
     def parse_items(self, response):
+        root_domain = 'https://www.theverge.com'
         verge_review = VergeReview()
         verge_review['url'] = response.url
         verge_review['title'] = response.xpath('//h1/text()').extract_first()
         verge_review['author_name'] = response.xpath("//span[contains(@class, '_114qu8c2')]/a/text()").extract_first()
-        verge_review['author_profile_link'] = response.xpath("//span[contains(@class, '_114qu8c2')]/a/@href").extract_first()
+        verge_review['author_profile_link'] = urljoin(root_domain, response.xpath("//span[contains(@class, '_114qu8c2')]/a/@href").extract_first())
 
         yield verge_review
