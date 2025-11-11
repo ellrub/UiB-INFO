1cc1994a517a8d4b85bf36027b6898bfffa4a0c0
Ruben Nienhuis <ellefsen88@gmail.com>
2025-11-11 23:37:17 +0100

    .

---
+++ notes.md
@@ -0,0 +1,2 @@
+Bruk VARCHAR når lengden på dataen varierer mye for å spare plass
+CHAR vil alltid bruke den fulle forhåndsdefinerte lengden
---
+++ oblig7.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(255),
+    ContactName VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    PostalCode VARCHAR(255),
+    Country VARCHAR(255)
+);
