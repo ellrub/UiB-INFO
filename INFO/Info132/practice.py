d0aeae5045d1c01f742a4e92ff61102c21c5682b
Ruben Nienhuis <ellefsen88@gmail.com>
2025-12-16 13:35:28 +0100

    .

--- INFO/Info132/practice.py
+++ INFO/Info132/practice.py
@@ -1,32 +1,58 @@
 # Tema 7
 # Oppgave 1
 
-friends = [
-    ['Ole',99887766],['Liv',99778899],['Gro',99556644],
-    ['Tom',98675601],['Eva',98987665],['Jan',88997766]
-    ]
+# friends = [
+#     ['Ole',99887766],['Liv',99778899],['Gro',99556644],
+#     ['Tom',98675601],['Eva',98987665],['Jan',88997766]
+#     ]
 
-def find_tlf_num(name, tlf_list):
-    for friend in tlf_list:
-        if friend[0] == name:
-            print(f'{friend[1]}')
-            return
+# def find_tlf_nr(name, list):
+#     for friend in list:
+#         if friend[0] == name:
+#             print(friend[1])
+#             return
+        
+#     print('Ukjent person')
 
-    print("Ukjent person")
+# find_tlf_nr("Gro", friends)
 
-find_tlf_num("Li", friends)
+# print(friends)
 
-friends_dict = {
-    'Ole': 99887766, 'Liv': 99778899, 'Gro': 99556644,
-    'Tom': 98675601, 'Eva': 98987665, 'Jan': 88997766
-}
+# def remove_nr(person, list):
+#     for friend in list:
+#         if friend[0] == person:
+#             list.remove(friend)
+#             print(list)
+#             return
+        
+#     print('Ukjent person')
 
-def find_tlf_dict(name, tlf_dict):
-    if name in tlf_dict:
-        print(tlf_dict[name])
-    else:
-        print("Ukjent person")
+# remove_nr("Liv", friends)
 
-find_tlf_dict("Jan", friends_dict)
+# Oppgave 2
+exam = {
+    'INFO100':'C', 'INFO104':'B', 'INFO116':'E',
+    'INFO180':'A', 'INFO201':'F','INFO280':'C',
+    'GEO101':'D', 'GEO110':'B','ADM101':'A',
+    'ECON100':'B', 'ECON201':'C','GEO210':'C',
+    'FAIL101':'F'
+    }
 
-def remove_person(person, tlf_list):
+def grade_frequence(exam_dict):
+    grade_frequence = {}
+    for course in exam_dict:
+        grade = exam_dict[course]
+        if grade in grade_frequence:
+            grade_frequence[grade] += 1
+        else:
+            grade_frequence[grade] = 1
+    return grade_frequence
+
+print(grade_frequence(exam))
+
+def grade_histogram(frequence):
+    for grade in sorted(frequence):
+        count = frequence[grade]
+        print(f'{grade}: {"*" * count}')
+
+grade_frequence(grade_frequence(exam))
